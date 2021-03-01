9dc41c791c01073b7a7079835186d3fbf4fbe3e9
priyanshu-shubham <priyanshushubham27@gmail.com>
2021-03-01 20:38:41 +0530

    Added GPU

--- u2net_load.py
+++ u2net_load.py
@@ -16,7 +16,7 @@ def model(model_name="u2net"):
     elif model_name == "u2netp":
         print("...load U2NEP---4.7 MB")
         net = U2NETP(3, 1)
-    net.load_state_dict(torch.load(model_dir, map_location=torch.device("cpu")))
+    net.load_state_dict(torch.load(model_dir))
 
     if torch.cuda.is_available():
         net.cuda()
